c8d2878af64f7784fc8621bd32efe6542a0f7a9a
Guillem Ballesteros <guillem.ballesteros.garcia@gmail.com>
2021-04-25 17:36:36 +0100

    Examples for all functions.

--- examples/timetrace.py
+++ examples/timetrace.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from time import perf_counter
 
 import matplotlib.pyplot as plt
@@ -10,22 +11,18 @@ ptu = trattoria.PTUFile(ptu_filepath)
 file_size = os.path.getsize(ptu_filepath) / (1024.0 ** 3)
 print(ptu)
 
-# Test G2
 start_time = perf_counter()
-g2_params = trattoria.G2Parameters(
-    channel_1=0,
-    channel_2=1,
-    correlation_window=50_000e-12,
-    resolution=600e-12,
-    start_record=None,
-    stop_record=None,
+timetrace_params = trattoria.TimeTraceParameters(
+    resolution=10.0,
+    channel=None,
 )
-g2_res = ptu.g2(g2_params)
+tt_res = ptu.timetrace(timetrace_params)
 end_time = perf_counter()
 time_delta = end_time - start_time
-print(f"G2 execution time: {time_delta:.3f} s")
+
+print(f"Timetrace execution time: {time_delta:.3f} s")
 print(f"  Processed {file_size/time_delta:.2f} GB/s")
 
-plt.plot(g2_res.t * 1e9, g2_res.g2)
-plt.xlabel("Delay (ns)")
+plt.plot(tt_res.t, tt_res.tt)
+plt.xlabel("Time (s)")
 plt.show()
--- examples/zerofinder.py
+++ examples/zerofinder.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from time import perf_counter
 
 import matplotlib.pyplot as plt
@@ -10,22 +11,19 @@ ptu = trattoria.PTUFile(ptu_filepath)
 file_size = os.path.getsize(ptu_filepath) / (1024.0 ** 3)
 print(ptu)
 
-# Test G2
 start_time = perf_counter()
-g2_params = trattoria.G2Parameters(
+zerofinder_params = trattoria.ZeroFinderParameters(
     channel_1=0,
     channel_2=1,
-    correlation_window=50_000e-12,
-    resolution=600e-12,
-    start_record=None,
-    stop_record=None,
 )
-g2_res = ptu.g2(g2_params)
+zf_res = ptu.zerofinder(zerofinder_params)
 end_time = perf_counter()
 time_delta = end_time - start_time
+
 print(f"G2 execution time: {time_delta:.3f} s")
 print(f"  Processed {file_size/time_delta:.2f} GB/s")
 
-plt.plot(g2_res.t * 1e9, g2_res.g2)
+plt.plot(zf_res.t * 1e9, zf_res.raw_hist)
+plt.plot(zf_res.t * 1e9, zf_res.fit_y)
 plt.xlabel("Delay (ns)")
 plt.show()
